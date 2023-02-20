3aa62fe27e79beb1f2d68566bb348d7de1ec0e40
Xisca04 <xiscacerdavicens@gmail.com>
2023-02-20 10:30:36 +0100

    targets_gameManager_uiText,_VFX

---
+++ Assets/Challenge 5/Scripts/Target.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Destrucción del game object al pasar un tiempo - AUTODESTRUCCIÓN
+
+    private float lifeTime = 2f;
+    public int points;
+    public GameObject explosionParticle;
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        Destroy(gameObject, lifeTime);
+    }
+
+    private void OnMouseDown() // Al hacer click se autodestruye
+    {
+        if (!gameManager.isGameOver)
+        {
+            if (gameObject.CompareTag("Bad"))
+            {
+                gameManager.isGameOver = true;
+            }
+            else if (gameObject.CompareTag("Good"))
+            {
+                gameManager.UpdateScore(points);
+            }
+
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        gameManager.targetPositionsInScene.Remove(transform.position);
+    }
+}
